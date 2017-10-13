a38f364fa2d8b66b43115e893295a36ecb1049a7
Pierre Kestener <pierre.kestener@cea.fr>
2017-10-13 10:25:48 +0200

    update monitoring information in the MPI case

--- src/shared/solver_utils.h
+++ src/shared/solver_utils.h
@@ -17,13 +17,31 @@ inline void print_solver_monitoring_info(SolverBase* solver)
   real_t t_bound = solver->timers[TIMER_BOUNDARIES]->elapsed();
   real_t t_io    = solver->timers[TIMER_IO]->elapsed();
 
-  printf("total       time : %5.3f secondes\n",t_tot);
-  printf("godunov     time : %5.3f secondes %5.2f%%\n",t_comp,100*t_comp/t_tot);
-  printf("compute dt  time : %5.3f secondes %5.2f%%\n",t_dt,100*t_dt/t_tot);
-  printf("boundaries  time : %5.3f secondes %5.2f%%\n",t_bound,100*t_bound/t_tot);
-  printf("io          time : %5.3f secondes %5.2f%%\n",t_io,100*t_io/t_tot);
-  
-  printf("Perf             : %5.3f number of Mcell-updates/s\n",solver->m_iteration*solver->m_nCells/t_tot*1e-6);
+  int myRank = 0;
+  int nProcs = 1;
+
+
+#ifdef USE_MPI
+
+  myRank = solver->params.myRank;
+  nProcs = solver->params.nProcs;
+
+  // only print on master
+  if (myRank == 0) {
+
+#endif // USE_MPI
+    
+    printf("total       time : %5.3f secondes\n",t_tot);
+    printf("godunov     time : %5.3f secondes %5.2f%%\n",t_comp,100*t_comp/t_tot);
+    printf("compute dt  time : %5.3f secondes %5.2f%%\n",t_dt,100*t_dt/t_tot);
+    printf("boundaries  time : %5.3f secondes %5.2f%%\n",t_bound,100*t_bound/t_tot);
+    printf("io          time : %5.3f secondes %5.2f%%\n",t_io,100*t_io/t_tot);
+    
+    printf("Perf             : %5.3f number of Mcell-updates/s\n",solver->m_iteration*solver->m_nCells*nProcs/t_tot*1e-6);
+    
+#ifdef USE_MPI
+  } // end myRank==0
+#endif
   
 } // print_solver_monitoring_info
 
