e10079e782fa06df8d1621476e43b44421dfb369
Pierre Kestener <pierre.kestener@cea.fr>
2025-01-03 17:04:15 +0100

    Apply cmake format and lint.

--- cmake/plantuml.cmake
+++ cmake/plantuml.cmake
@@ -1,14 +1,17 @@
 if(PLANTUML_FOUND)
-	return()
+  return()
 endif()
 
-find_program(PLANTUML_PATH
-	NAMES plantuml plantuml.bat
-	DOC "Path to PlantUML wrapper script.")
+find_program(
+  PLANTUML_PATH
+  NAMES plantuml plantuml.bat
+  DOC "Path to PlantUML wrapper script.")
 
 if(NOT PLANTUML_PATH)
-	message(FATAL_ERROR "Could not find PlantUML.")
+  message(FATAL_ERROR "Could not find PlantUML.")
 endif()
-set(PLANTUML_FOUND ON CACHE BOOL "Found PlantUML.")
+set(PLANTUML_FOUND
+    ON
+    CACHE BOOL "Found PlantUML.")
 mark_as_advanced(PLANTUML_FOUND)
 message(STATUS "Found PlantUML: ${PLANTUML_PATH}")
--- cmake/preventBuildInSource.cmake
+++ cmake/preventBuildInSource.cmake
@@ -1,3 +1,6 @@
 if(${CMAKE_SOURCE_DIR} STREQUAL ${CMAKE_BINARY_DIR})
-  message(FATAL_ERROR "In-source builds not allowed. Please make a new directory (called a build directory) and run CMake from there. You may need to remove CMakeCache.txt. ")
+  message(
+    FATAL_ERROR
+      "In-source builds not allowed. Please make a new directory (called a build directory) and run CMake from there. You may need to remove CMakeCache.txt. "
+  )
 endif()
